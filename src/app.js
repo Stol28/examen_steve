48149476567a322d333d802e98e76ec41435a7f8
Steve Pasche <steve.pasche@heig-vd.com>
2024-02-05 14:12:17 +0100

    code fix

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 "use strict";
-import User from './modules/User.js';
+import User from "./modules/User.js";
 
 let users = []; // Stocke les instances de User
 
 async function fetchUsers() {
-  const response = await fetch('https://randomuser.me/api/?results=20');
+  const response = await fetch("https://randomuser.me/api/?results=20");
   const data = await response.json();
-  users = data.results.map(userData => new User(userData)); // Crée et stocke les instances de User
+  users = data.results.map((userData) => new User(userData)); // Crée et stocke les instances de User
   displayUsers(sortByName()); // Affiche les utilisateurs initialement par nom
 }
 
 function displayUsers() {
-  const main = document.querySelector('main');
-  main.innerHTML = ''; // Vide le main avant de réafficher les utilisateurs
-  users.forEach(user => {
+  const main = document.querySelector("main");
+  main.innerHTML = ""; // Vide le main avant de réafficher les utilisateurs
+  users.forEach((user) => {
     user.generateUserElement();
     user.render();
-    updateButtonState('sort--name');
+    updateButtonState("sort--name");
   });
 }
 
@@ -31,21 +31,21 @@ function sortByName() {
 }
 
 // Ajout des écouteurs d'événements aux boutons
-document.getElementById('sort--name').addEventListener('click', () => {
+document.getElementById("sort--name").addEventListener("click", () => {
   sortByName();
-  updateButtonState('sort--name');
+  updateButtonState("sort--name");
 });
 
-document.getElementById('sort--age').addEventListener('click', () => {
+document.getElementById("sort--age").addEventListener("click", () => {
   sortByAge();
-  updateButtonState('sort--age');
+  updateButtonState("sort--age");
 });
 
 function updateButtonState(selectedButtonId) {
-  document.querySelectorAll('button').forEach(button => {
-    button.classList.remove('selected');
+  document.querySelectorAll("button").forEach((button) => {
+    button.classList.remove("selected");
   });
-  document.getElementById(selectedButtonId).classList.add('selected');
+  document.getElementById(selectedButtonId).classList.add("selected");
 }
 
 fetchUsers();
