e3d90aec571d4f9c48d463d84efaef305dc55a94
Steve Pasche <steve.pasche@heig-vd.com>
2024-02-05 12:15:08 +0100

    sorted by last name

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 "use strict";
 import User from './modules/User.js';
 
-
 async function fetchUsers() {
   const response = await fetch('https://randomuser.me/api/?results=20');
   const data = await response.json();
-  data.results.forEach(userData => {
-    const user = new User(userData);
+  const users = data.results.map(userData => new User(userData));
+  // Trier les utilisateurs par nom de famille
+  users.sort((a, b) => a.lastName.localeCompare(b.lastName));
+  users.forEach(user => {
     user.generateUserElement();
     user.render();
   });
--- src/modules/User.js
+++ src/modules/User.js
@@ -1,14 +1,14 @@
-
+// User.js
 export default class User {
-  constructor(user) {
-    this.title = user.name.title;
-    this.firstName = user.name.first;
-    this.lastName = user.name.last;
-    this.city = user.location.city;
-    this.country = user.location.country;
-    this.age = user.dob.age;
-    this.email = user.email;
-    this.picture = user.picture.large;
+  constructor(userData) {
+    this.title = userData.name.title;
+    this.firstName = userData.name.first;
+    this.lastName = userData.name.last;
+    this.city = userData.location.city;
+    this.country = userData.location.country;
+    this.age = userData.dob.age;
+    this.email = userData.email;
+    this.picture = userData.picture.large;
     this.isPresent = false; // Par défaut, l'utilisateur n'est pas présent
     this.userElement = null; // Sera défini lors de la génération de l'élément
   }
@@ -18,36 +18,38 @@ export default class User {
     this.userElement.classList.add("user");
     this.userElement.setAttribute("data-present", this.isPresent.toString());
     const userHTML = `
-      <img src="${this.picture}">
-      <div class="user--info">
-        <h1>${this.title} ${this.firstName} ${this.lastName}</h1>
-        <p>${this.age} years old</p>
-        <p>${this.city}, ${this.country}</p>
-      </div>
-      <a href="mailto:${this.email}">
-        <span class="mail">✉️</span>
-      </a>
-    `;
+        <img src="${this.picture}">
+        <div class="user--info">
+          <h1>${this.title} ${this.firstName} ${this.lastName}</h1>
+          <p>${this.age} years old</p>
+          <p>${this.city}, ${this.country}</p>
+        </div>
+        <a href="mailto:${this.email}">
+          <span class="mail">✉️</span>
+        </a>
+      `;
     this.userElement.insertAdjacentHTML("afterbegin", userHTML);
-    this.userElement.addEventListener("click", () => this.togglePresence());
+    this.addClickListener();
   }
 
   render() {
     const mainElement = document.querySelector("main");
-    if (mainElement && this.userElement) {
-      mainElement.appendChild(this.userElement);
-    }
+    mainElement.appendChild(this.userElement);
   }
 
   togglePresence() {
     this.isPresent = !this.isPresent;
     this.userElement.setAttribute("data-present", this.isPresent.toString());
-    if (this.isPresent) {
-      this.userElement.style.backgroundColor = "#06d6a0";
-      this.userElement.style.color = "#ffffff";
-    } else {
-      this.userElement.style.backgroundColor = "";
-      this.userElement.style.color = "";
-    }
+    this.userElement.style.backgroundColor = this.isPresent
+      ? "#06d6a0"
+      : "#ffffff";
+    const pElements = this.userElement.querySelectorAll("p");
+    pElements.forEach((p) => {
+      p.style.color = this.isPresent ? "#ffffff" : "#000000";
+    });
+  }
+
+  addClickListener() {
+    this.userElement.addEventListener("click", () => this.togglePresence());
   }
 }
